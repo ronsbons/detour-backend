54b96f1f38128c3f83461f6d064ee7a852c2c0f5
ronsbons <ronnilouie@gmail.com>
2019-03-31 11:38:31 -0700

    begins project set up

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// const routes = require('./routes/user.js');
+// const db = require('./models');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }) );
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.use('/user', userRoutes);
+
+// serve the 'public' folder in the frontend which has 'index.html', which index.js puts components into
+app.use(express.static('public'));
+
+// test response to send to front end when it connects to backend
+app.get('/', (request, response) => response.send('hello, backend'));
+
+
+app.listen(process.env.PORT || 3001, () =>
+  console.log('Listening on port 3001 :)')
+);
