04a765f8e06864a06e8dcc4a2f376121ee502b17
ronsbons <ronnilouie@gmail.com>
2019-04-02 10:40:49 -0700

    adds show all countries and show one country routes

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,6 +1,28 @@
 const db = require('../models');
 
 module.exports = {
+  // show all countries
+  show: (request, response) => {
+    db.Country.find({}, (error, foundCountries) => {
+      if (error) {
+        console.log(`can't get all countries: ${error}`);
+      }
+      response.json(foundCountries);
+      console.log(`found all countries ${foundCountries}`);
+    });
+  },
+
+  // find one country by its id
+  showOne: (request, response) => {
+    db.Country.find({_id: request.params.id}, (error, foundCountry) => {
+      if (error) {
+        console.log(`can't find one country: ${error}`);
+      }
+      response.json(foundCountry);
+      console.log(`found one country: ${foundCountry}`);
+    });
+  },
+
   // find countries by region
   showByRegion: (request, response) => {
     db.Country.find({region_id: request.params.region_id}, (error, foundCountries) => {
--- routes/country.js
+++ routes/country.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers');
 
+// get all countries
+router.get('/', controllers.country.show);
+// get one country by its id
+router.get('/:id', controllers.country.showOne);
 // get countries by region id
-router.get('/:region_id', controllers.country.showByRegion);
+router.get('/region/:region_id', controllers.country.showByRegion);
 
 module.exports = router;
